08f2ab6aacb9f13ec0228f159e274ca33835abb1
hu188 <18851151385@139.com>
2018-12-19 14:01:39 +0800

    Initial Commit

---
+++ Components/tabbar/index.js
@@ -0,0 +1,71 @@
+// components/tabbar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+   current:{
+     type: String,
+     value:'homepage'
+   }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleChange (e) {
+      const {key} = e.detail
+      if (key ==='order') {
+       
+        wx.switchTab({
+          url: '/index/two/two',
+        })
+      } else if (key ==='mine'){
+        wx.switchTab({
+          url: '/index/one/one',
+        })
+      } else if (key === 'homepage'){
+        wx.switchTab({
+          url: '/index/index',
+        })
+      }else{
+        wx.scanCode({
+          success: (res) => {
+            const { result } = res
+            if (result.length > 0) {
+              const str = result.split(':')[2]
+              if (str === undefined) {
+                $Toast({
+                  content: '扫码失败',
+                  type: 'error'
+                });
+              } else {
+                if (str.indexOf('//') === -1) {
+                  wx.switchTab({
+                    url: '../goods/index'
+                  })
+                } else {
+                  const id = str.split('=')[1].replace('//', '')
+                  wx.navigateTo({
+                    url: `../goodsDetail/index?id=${id}`
+                  })
+                }
+              }
+            }
+          },
+          fail: (res) => {
+          }
+        });
+      }
+      
+    }
+  }
+})
